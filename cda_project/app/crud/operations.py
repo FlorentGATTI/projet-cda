4d98f96a6e203874a592122e368059b52c7731af
georgesim1 <georgesimak@gmail.com>
2024-08-06 11:31:32 +0200

    added mongodb link

--- cda_project/app/crud/operations.py
+++ cda_project/app/crud/operations.py
@@ -1,8 +1,19 @@
-from sqlalchemy.orm import Session
 from app.models.prenoms import Prenom
+from pymongo.database import Database
 
-def get_total_births_by_year(db: Session, year: int):
-    return db.query(Prenom).filter(Prenom.year == year).count()
+def get_total_births_by_year(db: Database, year: int):
+    return db["prenoms"].count_documents({"year": year})
 
-def get_top_names_by_year(db: Session, year: int, top_n: int = 10):
-    return db.query(Prenom).filter(Prenom.year == year).order_by(Prenom.count.desc()).limit(top_n).all()
+def get_top_names_by_year(db: Database, year: int, top_n: int = 10):
+    cursor = db["prenoms"].find({"year": year}).sort("count", -1).limit(top_n)
+    return list(cursor)
+
+# from sqlalchemy.orm import Session
+# from app.models.prenoms import Prenom
+
+# def get_total_births_by_year(db: Session, year: int):
+#     return db.query(Prenom).filter(Prenom.year == year).count()
+
+# def get_top_names_by_year(db: Session, year: int, top_n: int = 10):
+#     return db.query(Prenom).filter(Prenom.year == year).order_by(Prenom.count.desc()).limit(top_n).all()
+
